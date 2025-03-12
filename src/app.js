f6bcb6535abbed4db988e440546a0d2a0d31637a
Vbarbosa <131600563+VitoriaBSouza@users.noreply.github.com>
2025-03-12 21:46:38 +0000

    New Generator Excuses

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function(){
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'on my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let RandomWords = (str) => { return Math.floor(Math.random()*str.length);}
+
+  let getExcuse = who[RandomWords(who)]+" "+action[RandomWords(action)]+" "+what[RandomWords(what)]+" "+when[RandomWords(when)]+".";
+
+  document.getElementById("excuse").innerHTML = getExcuse;
 };
